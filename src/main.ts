469e10f50d437114b71ce7f36e61185d4ba8093b
Tun Min <tunmin2000@gmail.com>
2023-05-18 09:17:42 +0700

    Implemented Swragger for OpenAPI

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('Shorten URL Application')
+  .setDescription('This App is build with NestJs for Shorten url.')
+  .setVersion('1.0')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/doc', app, document);
+
   app.useGlobalPipes( new ValidationPipe({ whitelist : true }) )
   await app.listen(process.env.PORT);
 }
--- src/modules/url/url.dto.ts
+++ src/modules/url/url.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from "@nestjs/swagger/dist";
 import { IsString, IsNotEmpty, IsDate, IsDateString } from "class-validator";
 
 export class ShortenUrlTdo {
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     longUrl : string; 
 
+    @ApiProperty()
     @IsDateString()
     @IsNotEmpty()
     expiry : string;
